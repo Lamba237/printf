f4a22de97c4cbc158d8394dd5153d7fb662b6229
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-10-17 17:07:59 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <math.h>
+
+int _printf(const char *format, ...);
+int _pow(int x, int y);
+
+#endif
